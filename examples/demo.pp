a9a08aa7ce51b75225cc938ca066247336f6fcf2
Jordi Prats <jordi.prats@gmail.com>
2019-05-27 10:21:29 +0200

    demo

--- examples/demo.pp
+++ examples/demo.pp
@@ -7,3 +7,10 @@ informatica::checkrepository { 'repo_demo':
   adminuser_password => 'assword',
   domain             => 'demo_domain',
 }
+
+informatica::checknodes { 'inf_domain':
+  adminuser_sd       => 'Native',
+  adminuser          => 'Administrator',
+  adminuser_password => 'assword',
+  expected_alive_nodes => '2',
+}
