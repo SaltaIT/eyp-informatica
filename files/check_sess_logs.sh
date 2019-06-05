76650ffb8a412a40a6d3076d49f09054c90f9859
Jordi Prats <jordi.prats@gmail.com>
2019-06-05 14:20:05 +0200

    checks

--- files/check_sess_logs.sh
+++ files/check_sess_logs.sh
@@ -23,33 +23,45 @@ fi
 
 if ! [[ "${BH_START}" =~ ^[0-9]+$ ]];
 then
-    echo "BH_START not an integer -->${BH_START}<--"  >&2
-    exit 3
+  echo "BH_START not an integer -->${BH_START}<--"  >&2
+  exit 3
 fi
 
 if ! [[ "${BH_END}" =~ ^[0-9]+$ ]];
 then
-    echo "BH_END not an integer -->${BH_END}<--"  >&2
-    exit 3
+  echo "BH_END not an integer -->${BH_END}<--"  >&2
+  exit 3
 fi
 
-LAST_LOG_TS=$(ls -tl --time-style='+%s' "${SESS_LOGS_DIR}" | head -n1 | awk '{ print $6 }')
+if ! [[ "${BH_MAX_HOURS}" =~ ^[0-9]+$ ]];
+then
+  echo "BH_MAX_HOURS not an integer -->${BH_MAX_HOURS}<--"  >&2
+  exit 3
+fi
+
+if ! [[ "${GLOBAL_MAX_HOURS}" =~ ^[0-9]+$ ]];
+then
+  echo "GLOBAL_MAX_HOURS not an integer -->${GLOBAL_MAX_HOURS}<--"  >&2
+  exit 3
+fi
+
+LAST_LOG_TS=$(ls -tl --time-style='+%s' "${SESS_LOGS_DIR}" | head -n2 | tail -n1 | awk '{ print $6 }')
 CURRENT_TS=$(date +%s)
 CURRENT_HOUR=$(date +%H)
 
-if [ "$((${CURRENT_TS}-${LAST_LOG_TS}))" -gt "$((24*60*60))" ];
+if [ "$((${CURRENT_TS}-LAST_LOG_TS))" -gt "$((${GLOBAL_MAX_HOURS}*60*60))" ];
 then
-        echo "No logs have been generated in the last 24h"
-        exit 2
+  echo "No logs have been generated in the last ${GLOBAL_MAX_HOURS} hours"
+  exit 2
 fi
 
 # within BH ensure at least a file in generated every hour
 if [ "${CURRENT_HOUR}" -gt "${BH_START}" ] && [ "${CURRENT_HOUR}" -le "${BH_END}" ];
 then
-  if [ "$((${CURRENT_TS}-${LAST_LOG_TS}))" -gt "$((60*60))" ];
+  if [ "$((${CURRENT_TS}-LAST_LOG_TS))" -gt "$((${BH_MAX_HOURS}*60*60))" ];
   then
-          echo "No logs have been generated within the last hour"
-          exit 2
+    echo "No logs have been generated within the last ${BH_MAX_HOURS} hour(s)"
+    exit 2
   fi
 fi
 
